156feaf52d7f3cbfbc0848e34f792dbca31b6c68
yashsharma2007 <yashpraveen2007@gmail.com>
2026-05-06 23:30:25 +0530

    medical store website

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+function App() {
+  return (
+    <div style={{fontFamily:"Arial", padding:"20px"}}>
+    <h1>Deepak Medical Store</h1>
+
+      <h2>Your Trusted Medical Store</h2>
+
+      <p>
+        Fast delivery • Genuine medicines • Healthcare products
+      </p>
+
+      <hr />
+
+      <h3>Our Services</h3>
+
+      <ul>
+        <li>Medicine Delivery</li>
+        <li>Prescription Support</li>
+        <li>Healthcare Products</li>
+        <li>OTC Medicines</li>
+      </ul>
+
+      <hr />
+
+      <h3>Contact Us</h3>
+
+      <p>Phone: +91 9350393521</p>
+
+      <p>Location: A-530 sec-9 new vijay nagar Ghaziabad</p>
+
+      <button>
+        Order on WhatsApp
+      </button>
+    </div>
+  );
+}
+
+export default App;
